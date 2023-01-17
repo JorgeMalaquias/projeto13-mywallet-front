ded2408afd062f8803db819846c3126479810dad
Jorge Malaquias <jorgemalaquia@gmail.com>
2023-01-17 00:18:04 -0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -17,7 +17,7 @@ function registering(e,name,setName,email,setEmail,password,setPassword,password
             email,
             password
         }
-        axios.post(`${process.env.REACT_APP_API}/sign-up`,body).then((r)=>{
+        axios.post(`https://my-wallet-api-a47v.onrender.com/sign-up`,body).then((r)=>{
 
             alert('Cadastro feito com sucesso. Para acessar sua conta faça o login');
             setName('');
