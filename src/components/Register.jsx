bf3da34a3059f9b84277709a782779c101e0f4ae
Jorge Malaquias <jorgemalaquia@gmail.com>
2023-01-16 21:20:37 -0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -17,7 +17,7 @@ function registering(e,name,setName,email,setEmail,password,setPassword,password
             email,
             password
         }
-        axios.post(`https://backendsdriven.herokuapp.com/sign-up`,body).then((r)=>{
+        axios.post(`${process.env.API}/sign-up`,body).then((r)=>{
 
             alert('Cadastro feito com sucesso. Para acessar sua conta faça o login');
             setName('');
